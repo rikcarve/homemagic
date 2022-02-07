0a05c0fda0d6fb6a2e110bd1fc41d8324c7844c2
rikcarve <arikdasen@outlook.com>
2022-02-07 18:22:08 +0100

    add arm64 support

--- src/main/java/ch/carve/homemagic/model/YeelightController.java
+++ src/main/java/ch/carve/homemagic/model/YeelightController.java
@@ -6,6 +6,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 public class YeelightController implements SwitchController {
@@ -36,11 +38,13 @@ public class YeelightController implements SwitchController {
     private void sendMessage(String message, String ip, int port) {
         try {
             Socket socket = new Socket(ip, port);
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), false, StandardCharsets.UTF_8);
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            out.println(message);
+            out.print(message + "\r\n");
+            out.flush();
             String response = in.readLine();
             log.info("Response: " + response);
+            socket.close();
         } catch (Exception e) {
             log.warn("Could not send msg: {}", message, e);
         }
